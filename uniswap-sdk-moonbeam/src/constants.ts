1312c4f4b27ecb83c1a816c6828c94328de6a1d0
bestsoftdev2020 <bestsoftdev2020@yandex.ru>
2021-10-31 05:36:17 +0300

    uniswap fork  moonbean contracts interface sdk

---
+++ uniswap-sdk-moonbeam/src/constants.ts
@@ -0,0 +1,56 @@
+import JSBI from 'jsbi'
+import { factory } from './moonbase_address.json'
+
+// exports for external consumption
+export type BigintIsh = JSBI | bigint | string
+
+export enum ChainId {
+  MAINNET = 1,
+  STANDALONE = 1281,
+  MOONROCK = 1286,
+  MOONBASE = 1287,
+  MOONSHADOW = 1288
+}
+
+export enum TradeType {
+  EXACT_INPUT,
+  EXACT_OUTPUT
+}
+
+export enum Rounding {
+  ROUND_DOWN,
+  ROUND_HALF_UP,
+  ROUND_UP
+}
+
+export const FACTORY_ADDRESS: { [key: string]: string } = {
+  [ChainId.STANDALONE]: '0x5c4242beB94dE30b922f57241f1D02f36e906915',
+  [ChainId.MOONROCK]: factory,
+  [ChainId.MOONBASE]: factory,
+  [ChainId.MOONSHADOW]: factory
+}
+
+export const INIT_CODE_HASH = '0x01429e880a7972ebfbba904a5bbe32a816e78273e4b38ffa6bdeaebce8adba7c'
+
+export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
+
+// exports for internal consumption
+export const ZERO = JSBI.BigInt(0)
+export const ONE = JSBI.BigInt(1)
+export const TWO = JSBI.BigInt(2)
+export const THREE = JSBI.BigInt(3)
+export const FIVE = JSBI.BigInt(5)
+export const TEN = JSBI.BigInt(10)
+export const _100 = JSBI.BigInt(100)
+export const _997 = JSBI.BigInt(997)
+export const _1000 = JSBI.BigInt(1000)
+
+export enum SolidityType {
+  uint8 = 'uint8',
+  uint256 = 'uint256'
+}
+
+export const SOLIDITY_TYPE_MAXIMA = {
+  [SolidityType.uint8]: JSBI.BigInt('0xff'),
+  [SolidityType.uint256]: JSBI.BigInt('0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff')
+}
